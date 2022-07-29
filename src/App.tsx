c2f23af0f0688517a3fc84b5129e5904a524b8cc
Sahid Rahman <sahidrahman404@gmail.com>
2022-07-29 14:50:30 +0700

    add useEffect to save the name value to the local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
-function App() {}
+function Greeting({ initialName = "" }: { initialName?: string }) {
+  const [name, setName] = useState<string>(
+    window.localStorage.getItem("name") || initialName
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("name", name);
+  }, [name]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(event: FormEvent<HTMLFormElement>) => event.preventDefault()}
+      >
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
+    </div>
+  );
+}
+
+function App() {
+  return <Greeting initialName="rahman" />;
+}
 
 export default App;
