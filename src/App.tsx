08a2c44e7f591b02142277b2c7220fdd18026fbb
Sahid Rahman <sahidrahman404@gmail.com>
2022-07-30 07:05:57 +0700

    add useLocalStorageWithState custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
-function Greeting({ initialName = "" }: { initialName?: string }) {
-  const [name, setName] = useState<string>(
-    () => window.localStorage.getItem("name") || initialName
-  );
+interface SerializeAndDeserialize {
+  serialize: <T>(a: T) => T;
+  deserialize: <T>(a: T) => T;
+}
+
+const useLocalStorageWithState = (
+  key: string,
+  initialProps?: unknown,
+  options?: SerializeAndDeserialize
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  const defaultOptions = {
+    serialize: JSON.stringify,
+    deserialize: JSON.parse,
+  };
+
+  const [state, setState] = useState<string>(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      return options
+        ? options.deserialize(valueInLocalStorage)
+        : defaultOptions.deserialize(valueInLocalStorage);
+    }
+
+    return typeof initialProps === "function" ? initialProps() : "";
+  });
 
   useEffect(() => {
-    window.localStorage.setItem("name", name);
-  }, [name]);
+    window.localStorage.setItem(
+      key,
+      options ? options.serialize(state) : defaultOptions.serialize(state)
+    );
+  }, [options, key, state]);
+
+  return [state, setState];
+};
+
+function Greeting({ initialName = "" }: { initialName?: string }) {
+  const [name, setName] = useLocalStorageWithState("name");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
